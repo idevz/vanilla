d76db0de4e37022ccbc86915e25c78bb9ea2eacf
idevz <15134559390@163.com>
2015-08-28 00:39:00 +0800

    Bootstrap:bootstrap()
    Bootstrap:bootstrap() use v(self) for development use pcall(v, self)
    for production

--- vanilla/v/bootstrap.lua
+++ vanilla/v/bootstrap.lua
@@ -19,13 +19,9 @@ end
 function Bootstrap:bootstrap()
 	for k,v in pairs(self.boot_list()) do
 		v(self)
+		-- @todo request_or_error
 		-- local ok, request_or_error = pcall(v, self)
 	end
-end
-
-function Bootstrap:t()
-	pp(self)
-	ngx.say('ttttt------------------ttttt')
 end
 
 return Bootstrap
