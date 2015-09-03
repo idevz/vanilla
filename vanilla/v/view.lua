6f0a0fd98e50df89e82a742a4b97b8da186e6bdc
idevz <15134559390@163.com>
2015-09-04 01:44:52 +0800

    view interface
    a view instance contain those args:controller_name, action, view_config

---
+++ vanilla/v/view.lua
@@ -0,0 +1,31 @@
+-- perf
+local error = error
+local setmetatable = setmetatable
+
+
+local View = {}
+View.__index = View
+
+function View:new(controller_name, action, view_config)
+end
+
+function View:assign()
+end
+
+function View:caching()
+end
+
+function View:display()
+end
+
+function View:getScriptPath()
+end
+
+function View:render()
+end
+
+function View:setScriptPath()
+end
+
+
+return View
