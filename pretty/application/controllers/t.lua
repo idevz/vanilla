9ede1fdf4cdffb072d638af9158c59621529111d
idevz <15134559390@163.com>
2015-09-07 15:30:58 +0800

    realize  a router
    a router named simple

---
+++ pretty/application/controllers/t.lua
@@ -0,0 +1,31 @@
+local helpers = require 'gin.helpers.common'
+local TController = {}
+
+function TController:index()
+	local view = self:getView()
+	-- view:render('index/index.html', {message = '============'})
+	-- pp(view:caching(true))
+	local resp = self:getResponse()
+	-- resp:setHeader('Content_type', 'application/json')
+	view:assign('message', '-----TTTT------')
+	view:assign('kk', '------Index-----')
+	-- view:assign()
+	view:display()
+	-- return 200, { message = "-------!" .. self.params.p }
+end
+
+function TController:aa()
+	local view = self:getView()
+	-- view:render('index/index.html', {message = '============'})
+	-- pp(view:caching(true))
+	local resp = self:getResponse()
+	-- resp:setHeader('Content_type', 'application/json')
+	pp(self.params)
+	view:assign('message', '-----TTTT------')
+	view:assign('kk', '------Aa-----')
+	-- view:assign()
+	view:display()
+	-- return 200, { message = "-------!" .. self.params.p }
+end
+
+return TController
--- vanilla/v/routes/simple.lua
+++ vanilla/v/routes/simple.lua
@@ -1,3 +1,6 @@
+-- perf
+local sgmatch = string.gmatch
+
 -- init Simple and set routes
 local Simple = {}
 
@@ -14,44 +17,20 @@ end
 
 function Simple:match()
     local uri = self.request.uri
-    local method = self.request.method
-
-
-    return './index', 'index'
-    -- return major_version .. '/' .. dispatcher[method].controller, dispatcher[method].action, params, request
-
-    -- -- match version based on headers
-    -- if self.request.headers['accept'] == nil then error({ code = 100 }) end
-
-    -- local major_version, rest_version = smatch(self.request.headers['accept'], accept_header_matcher)
-    -- if major_version == nil then error({ code = 101 }) end
-
-    -- local routes_dispatchers = Routes.dispatchers[tonumber(major_version)]
-    -- if routes_dispatchers == nil then error({ code = 102 }) end
-
-    -- -- loop dispatchers to find route
-    -- for i = 1, #routes_dispatchers do
-    --     local dispatcher = routes_dispatchers[i]
-    --     if dispatcher[method] then -- avoid matching if method is not defined in dispatcher
-    --         local match = { smatch(uri, dispatcher.pattern) }
-
-    --         if #match > 0 then
-    --             local params = {}
-    --             for j = 1, #match do
-    --                 if dispatcher[method].params[j] then
-    --                     params[dispatcher[method].params[j]] = match[j]
-    --                 else
-    --                     tappend(params, match[j])
-    --                 end
-    --             end
-
-    --             -- set version on request
-    --             self.request.api_version = major_version .. rest_version
-    --             -- return
-    --             return major_version .. '/' .. dispatcher[method].controller, dispatcher[method].action, params, request
-    --         end
-    --     end
-    -- end
+    local match = {}
+    local tmp = 1
+    if uri == '/' then
+        return 'index', 'index'
+    end
+    for v in sgmatch(uri , '/([A-Za-z0-9_]+)') do
+        match[tmp] = v
+        tmp = tmp +1
+    end
+    if #match == 1 then
+        return match[1], 'index'
+    else
+        return table.concat(match, '.', 1, #match - 1), match[#match]
+    end
 end
 
 return Simple
