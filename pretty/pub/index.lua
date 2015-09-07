5451799cba324fdfccf4370fe5ded64b8982d9c2
idevz <15134559390@163.com>
2015-09-07 22:38:27 +0800

    content_by_lua could be cached
    content_by_lua could be cached and ngx was gone cause read a nil
    request body

--- pretty/pub/index.lua
+++ pretty/pub/index.lua
@@ -4,6 +4,10 @@ function pp( ... )
     helpers.pp_to_file(..., '/Users/zj-git/vanilla/pretty/zj')
 end
 
+function err_log(msg)
+    ngx.log(ngx.ERR, "===zjdebug" .. msg .. "===")
+end
+
 local config = require('config.application')
 local app = require('vanilla.v.application'):new(ngx, config)
 app:bootstrap():run()
