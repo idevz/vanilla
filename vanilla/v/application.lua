77cac2d37f9246cf9d74e2fd59ecc9b7e3c437a4
idevz <15134559390@163.com>
2015-09-11 07:45:02 +0800

    create lpcall a instance method
    self:lpcall(function() return self.dispatcher:dispatch() end)

--- vanilla/v/application.lua
+++ vanilla/v/application.lua
@@ -1,5 +1,6 @@
 -- perf
 local pairs = pairs
+local pcall = pcall
 local require = require
 local setmetatable = setmetatable
 local function tappend(t, v) t[#t+1] = v end
@@ -13,45 +14,36 @@ end
 
 local Application = {}
 
+function Application:lpcall( ... )
+    local ok, rs_or_error = pcall( ... )
+    if ok then
+        return rs_or_error
+    else
+        self:raise_syserror(rs_or_error)
+    end
+end
+
 function Application:new(ngx, config)
     self.ngx = ngx
     self.config = buildconf(config)
-    local ok, dispatcher_or_error = pcall(function() return require('vanilla.v.dispatcher'):new(self) end)
-    if ok then
-        dispatcher = dispatcher_or_error
-    else
-        self:raise_syserror(dispatcher_or_error)
-    end
     local instance = {
         run = self.run,
         bootstrap = self.bootstrap,
-        dispatcher = dispatcher
+        dispatcher = self:lpcall(function() return require('vanilla.v.dispatcher'):new(self) end)
     }
     setmetatable(instance, {__index = self})
     return instance
 end
 
 function Application:bootstrap()
-    local ok, bootstrap_or_error = pcall(function() return require('application.bootstrap'):new(self.dispatcher) end)
-    if ok then
-        bootstrap = bootstrap_or_error
-    else
-        self:raise_syserror(bootstrap_or_error)
-    end
-
-    local ok, bootstarp_or_error = pcall(function() return bootstrap:bootstrap() end)
-    if ok == false then
-        self:raise_syserror(bootstarp_or_error)
-    end
+    bootstrap = self:lpcall(function() return require('application.bootstrap'):new(self.dispatcher) end)
+    self:lpcall(function() return bootstrap:bootstrap() end)
     
     return self
 end
 
 function Application:run()
-    local ok, status_or_error = pcall(function() return self.dispatcher:dispatch() end)
-    if ok == false then
-        self:raise_syserror(status_or_error)
-    end
+    self:lpcall(function() return self.dispatcher:dispatch() end)
 end
 
 function Application:raise_syserror(err)
