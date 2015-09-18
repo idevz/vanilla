f5cde661836e6505643d0bc549a45445677eb6ed
idevz <15134559390@163.com>
2015-09-18 17:14:48 +0800

    rm a test demo pretty

--- pretty/nginx/init.lua
+++
@@ -1,3 +0,0 @@
-return function ( ... )
-	ngx.say(...)
-end
--- pretty/reload.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-nginx -p /Users/zj-git/vanilla/pretty/ -c tmp/nginx.conf -s stop
-nginx -p /Users/zj-git/vanilla/pretty/ -c tmp/nginx.conf
