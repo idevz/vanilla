9aed49bfb11b1a638819ec677fc8ef65d8244453
idevz <15134559390@163.com>
2016-01-15 00:02:04 +0800

    route err detail

--- vanilla/v/router.lua
+++ vanilla/v/router.lua
@@ -1,5 +1,6 @@
 -- perf
 local error = error
+local tconcat = table.concat
 local function tappend(t, v) t[#t+1] = v end
 
 -- init Router and set routes
@@ -46,14 +47,18 @@ end
 
 function Router:route()
     if #self.routes >= 1 then
-        for _,route in ipairs(self.routes) do
+        local route_err = {}
+        for k,route in ipairs(self.routes) do
             local ok, controller_name_or_error, action = pcall(route_match, route)
             if ok and controller_name_or_error then
                 self.current_route = route
                 return controller_name_or_error, action
+            else
+                route_err[k] = controller_name_or_error
             end
         end
-        error({ code = 202, msg = {Routes_No_Match = #self.routes .. "Routes All Didn't Match."}})
+        error({ code = 202, msg = {
+            Routes_No_Match = #self.routes .. "Routes All Didn't Match. Errs Like: " .. tconcat( route_err, ", ")}})
     end
     error({ code = 201, msg = {Empty_Routes = 'Null routes added.'}})
 end
