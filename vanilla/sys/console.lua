1175c164afe1761399b4f931d62c051a08aa43fc
idevz <15134559390@163.com>
2016-03-23 21:38:23 +0800

    use standard  lua for interactive console

--- vanilla/sys/console.lua
+++ vanilla/sys/console.lua
@@ -2,9 +2,9 @@ local Console = {}
 
 function Console.start()
 	local lua = 'lua'
-	if VANILLA_JIT_BIN ~= nil then
-		lua = VANILLA_JIT_BIN
-	end
+	-- if VANILLA_JIT_BIN ~= nil then
+	-- 	lua = VANILLA_JIT_BIN
+	-- end
     local cmd = lua .. " -i -e \"_PROMPT='v-console>';package.path='" .. package.path 
               .. "' package.cpath='" .. package.cpath 
               .. "' require 'vanilla.sys.config';require 'vanilla.v.libs.utils'\""
