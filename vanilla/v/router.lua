5e3135d458d1a884fd55598280c75af868d9fe8e
idevz <15134559390@163.com>
2016-04-11 15:20:09 +0800

    fix a route bug when restful router return a nil value

--- vanilla/v/router.lua
+++ vanilla/v/router.lua
@@ -51,8 +51,8 @@ function Router:route()
             if route then
                 alive_route_num = alive_route_num + 1
                 local ok, controller_name_or_error, action = pcall(route_match, route)
-                if ok and package.searchpath(ngx.var.document_root .. '/application/controllers.'
-                    .. controller_name_or_error, '/?.lua;/?/init.lua') ~=nil
+                if ok and controller_name_or_error ~= nil and package.searchpath(ngx.var.document_root .. '/application/controllers.'
+                    .. controller_name_or_error, '/?.lua;/?/init.lua') ~= nil
                     and type(require('controllers.' .. controller_name_or_error)[action]) == 'function' then
                 -- if ok and controller_name_or_error then
                     self.current_route = route
