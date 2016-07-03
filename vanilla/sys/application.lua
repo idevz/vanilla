a2315cc3aeb19fd509573c4202d421aeef0baf52
idevz <15134559390@163.com>
2016-07-03 22:52:11 +0800

    add page cache

--- vanilla/sys/application.lua
+++ vanilla/sys/application.lua
@@ -414,7 +414,13 @@ local APP_ROOT = Registry['APP_ROOT']
 local Appconf={}
 Appconf.sysconf = {
     'v_resource',
+    'cache'
 }
+Appconf.page_cache = {}
+Appconf.page_cache.cache_on = true
+-- Appconf.page_cache.cache_handle = 'lru'
+Appconf.page_cache.no_cache_cookie = 'va-no-cache'
+Appconf.page_cache.build_cache_key_without_args = {'rd'}
 Appconf.vanilla_root = '{{VANILLA_ROOT}}'
 Appconf.vanilla_version = '{{VANILLA_VERSION_DIR_STR}}'
 Appconf.name = '{{APP_NAME}}'
