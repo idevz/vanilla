63ad2b23da2b3d43b741895f8b892402631c4d0f
idevz <15134559390@163.com>
2016-07-12 18:15:34 +0800

    no_cache_uris

--- vanilla/sys/application.lua
+++ vanilla/sys/application.lua
@@ -421,7 +421,7 @@ Appconf.page_cache.cache_on = true
 -- Appconf.page_cache.cache_handle = 'lru'
 Appconf.page_cache.no_cache_cookie = 'va-no-cache'
 Appconf.page_cache.no_cache_uris = {
-    'cca'
+    'uris'
 }
 Appconf.page_cache.build_cache_key_without_args = {'rd'}
 Appconf.vanilla_root = '{{VANILLA_ROOT}}'
